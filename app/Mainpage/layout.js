5cc592e1b2288a4226158f470f278494058a417b
Sumit639 <sumitkkumar639@gmail.com>
2023-11-19 12:47:17 +0530

    Update layout.js
    added page transition

--- app/Mainpage/layout.js
+++ app/Mainpage/layout.js
@@ -1,8 +1,18 @@
+"use client";
+import { motion, AnimatePresence } from "framer-motion";
+
+// const variants = {
+//   hidden: { opacity: 0, x: -200, y: 0 },
+//   enter: { opacity: 1, x: 0, y: 0 },
+// };
+
 import Sidebar from "@/components/sidebar";
+import { useRouter } from "next/navigation";
 
 export default function MainpageLayout({
   children, // will be a page or nested layout
 }) {
+  const route = useRouter();
   return (
     <section>
       {/* Include shared UI here e.g. a header or sidebar */}
@@ -10,7 +20,17 @@ export default function MainpageLayout({
         <Sidebar />
 
         <span className="child">
-        {children}
+          <AnimatePresence mode="wait">
+            <motion.main
+              key={route.pathname}
+              initial={{ opacity: 0, y: 15 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 15 }}
+              transition={{ delay: 0.65 }}
+            >
+              {children}
+            </motion.main>
+          </AnimatePresence>
         </span>
       </div>
     </section>
