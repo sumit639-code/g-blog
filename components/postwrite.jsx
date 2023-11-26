308d9ebb44a7aab168d2543a5506096590809bd8
SubhashisBD <subhashisbhanjdeo@gmail.com>
2023-11-26 13:24:34 +0530

    style

--- components/postwrite.jsx
+++ components/postwrite.jsx
@@ -3,39 +3,20 @@ import React, { useEffect, useState } from "react";
 import supabase from "@/supabase";
 
 const postwrite = () => {
-  const [image, setimage] = useState();
-  const [Name, setName] = useState("Loading...");
-  useEffect(() => {
-    async function getProfiles() {
-      const { data, error } = await supabase.auth.getUser();
-      // const user = supabase.auth.getUser();
-      // let { dat, err, status } = await supabase
-      //   .from("profiles")
-      //   .select(`username`)
-      //   .eq("id", user.id)
-      //   .single();
-      //   console.log(dat,err,status)
-      // // supabase.auth
-      console.log(data.user.user_metadata);
-      // setimage(data.user.user_metadata.avatar_url);
-      setName(data.user.user_metadata.full_name);
-    }
-    getProfiles();
-  });
-  console.log(image);
   const [photos, setphotos] = useState("");
-
+  
   return (
     <>
       <div className="mainte">
         <div className="write">
-          <Image src="/vercel.svg" width={30} height={20} alt="avatar" />
-          <textarea name="text" id="content" cols="40" rows="2"></textarea>
+          <Image src="/vercel.svg" width={50} height={30} alt="avatar" />
+          <textarea name="text" id="content" cols="50" rows="3" placeholder="Write here....."></textarea>
         </div>
         <div className="func">
           <div className="funcin">
-            <div className="photos">photos</div>
-            <div className="url">Add url</div>
+            <Image src="/photo.svg" width={20} height ={20} alt="picture" />
+            <div className="photos">Photos</div>
+            {/* <div className="url">Add url</div> */}
           </div>
           <div className="post">Post</div>
         </div>
